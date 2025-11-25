ef64cb52a9926be34bc250e4d1b3fee79cdcbde7
Mrinal Jain <2mrinaljain@gmail.com>
2025-11-25 16:19:14 -0500

    fix copilot config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,31 +1,28 @@
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
-  build = ":Copilot auth",
-  event = "BufReadPost",
-  opts = {
-    suggestion = {
-      keymap = {
-        accept = false, -- handled by completion engine
-      },
-    },
-  },
-  specs = {
-    {
-      "AstroNvim/astrocore",
-      opts = {
-        options = {
-          g = {
-            -- set the ai_accept function
-            ai_accept = function()
-              if require("copilot.suggestion").is_visible() then
-                require("copilot.suggestion").accept()
-                return true
-              end
-            end,
-          },
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
         },
       },
-    },
-  },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+      },
+    })
+
+    -- Keybinding to toggle auto trigger
+    vim.keymap.set("n", "<leader>ct", function()
+      require("copilot.suggestion").toggle_auto_trigger()
+    end, { desc = "Toggle Copilot suggestions" })
+  end,
 }
